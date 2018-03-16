55dd4a9d61d0e51079f1ca2b15a10c4904c94319
Lucas Apoena <lucasapoena@gmail.com>
2018-03-16 15:44:17 -0300

    Implementeção do model - Usuário

---
+++ src/main/java/br/com/abutres/model/Usuario.java
@@ -0,0 +1,49 @@
+package br.com.abutres.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String login;
+	private String password;
+	private Membro membro;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Membro getMembro() {
+		return membro;
+	}
+	public void setMembro(Membro membro) {
+		this.membro = membro;
+	}
+	
+	
+}
