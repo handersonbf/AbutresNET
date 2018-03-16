e3652d084d9a7cc39a7df36e861768fe5b0d8e55
Lucas Apoena <lucasapoena@gmail.com>
2018-03-16 15:43:48 -0300

    Implementeção do model - Comunicado

---
+++ src/main/java/br/com/abutres/model/Comunicado.java
@@ -0,0 +1,49 @@
+package br.com.abutres.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Comunicado implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private Usuario usuarioAutor;
+	private String assunto;
+	private String mensagem;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Usuario getAutor() {
+		return usuarioAutor;
+	}
+	public void setAutor(Usuario usuarioAutor) {
+		this.usuarioAutor = usuarioAutor;
+	}
+	public String getAssunto() {
+		return assunto;
+	}
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+}
