6f5680179c2d920870316aa81d08545b4b18bc3f
Lucas Apoena <lucasapoena@gmail.com>
2018-03-26 13:13:08 -0300

    Inclusão da classe SedesService

--- src/main/java/br/com/abutres/controller/SedesController.java
+++ src/main/java/br/com/abutres/controller/SedesController.java
@@ -7,43 +7,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.abutres.model.Sede;
-import br.com.abutres.repository.SedeRepository;
+import br.com.abutres.service.SedesService;
 
 @Controller
 public class SedesController {
 	
 	@Autowired
-	private SedeRepository sedes;
+	private SedesService sedesService;
 	
 	
 	@GetMapping("/sedes")
 	public ModelAndView index() {
 		ModelAndView view = new ModelAndView("views/sedes/lista");
-		view.addObject("sedes", sedes.findAll());
+		view.addObject("sedes", sedesService.findAll());
 		view.addObject(new Sede());
 		return view;
 	}
 	
 	@PostMapping("/sedes")
 	public String salvar(Sede sede) {
-		this.sedes.save(sede);
+		this.sedesService.salvar(sede);
 		return "redirect:/sedes";
 
 	}
-	
-	/*
-	index
-
-	show
-
-	edit
-
-	delete
-
-	create
-
-	save
-
-	update
-*/
 }
