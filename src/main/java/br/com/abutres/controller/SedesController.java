5d5e3099cbac355293f3d3b35f3939a9c1138eca
Lucas Apoena <lucasapoena@gmail.com>
2018-03-26 21:58:42 -0300

    Modificada as actions padroes

--- src/main/java/br/com/abutres/controller/SedesController.java
+++ src/main/java/br/com/abutres/controller/SedesController.java
@@ -3,6 +3,7 @@ package br.com.abutres.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,16 +18,30 @@ public class SedesController {
 	
 	
 	@GetMapping("/sedes")
-	public ModelAndView index() {
+	public ModelAndView lista() {
 		ModelAndView view = new ModelAndView("views/sedes/lista");
 		view.addObject("sedes", sedesService.findAll());
+		return view;
+	}
+	
+	@GetMapping("/sedes/adiciona")
+	public ModelAndView adiciona() {
+		ModelAndView view = new ModelAndView("views/sedes/adiciona");
 		view.addObject(new Sede());
 		return view;
 	}
 	
-	@PostMapping("/sedes")
-	public String salvar(Sede sede) {
+	@PostMapping("/sedes/adiciona")
+	public String adiciona(Sede sede) {
 		this.sedesService.salvar(sede);
 		return "redirect:/sedes";
+	}
+	
+	@GetMapping("/sedes/exibe/{id}")
+	public ModelAndView exibe(@PathVariable("id") long id) {
+		Sede sede = sedesService.findById(id);
+		ModelAndView view = new ModelAndView("views/sedes/exibe");
+		view.addObject("sede", sede);
+		return view;
 	}
 }
--- src/main/java/br/com/abutres/repository/SedesRepository.java
+++ src/main/java/br/com/abutres/repository/SedesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.abutres.model.Sede;
 
 public interface SedesRepository extends JpaRepository<Sede, Long> {
-
+	
 }
