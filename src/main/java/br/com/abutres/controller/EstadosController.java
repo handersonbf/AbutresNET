47b28fcf05b98b8daed1023918b1b30c0609d069
Lucas Apoena <lucasapoena@gmail.com>
2018-03-29 08:30:51 -0300

    Editado a action de adicionar estado

--- src/main/java/br/com/abutres/controller/EstadosController.java
+++ src/main/java/br/com/abutres/controller/EstadosController.java
@@ -1,20 +1,28 @@
 package br.com.abutres.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.abutres.model.Estado;
 import br.com.abutres.service.EstadosService;
+import br.com.abutres.service.PaisesService;
 
 @Controller
 public class EstadosController {
 	
 	@Autowired
 	private EstadosService estados;
+	
+	@Autowired
+	private PaisesService paises;
 		
 	@GetMapping("/estados")
 	public ModelAndView listar() {
@@ -24,16 +32,24 @@ public class EstadosController {
 	}
 	
 	@GetMapping("/estados/adicionar")
-	public ModelAndView adicionar() {
+	public ModelAndView adicionar(Estado estado) {
 		ModelAndView view = new ModelAndView("views/estados/adicionar");
-		view.addObject(new Estado());
+		view.addObject("paises", paises.findAll()); 
 		return view;
 	}
 	
 	@PostMapping("/estados/adicionar")
-	public String adicionar(Estado estado) {
+	public ModelAndView salvar(@Valid Estado estado, BindingResult result
+			, RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			return this.adicionar(estado);
+		}
 		this.estados.salvar(estado);
-		return "redirect:/estados";
+		
+		ModelAndView mv = new ModelAndView("redirect:/estados/adicionar");
+		attributes.addFlashAttribute("mensagem", "Estado salvo com sucesso.");
+		return mv;
 	}
 	
 	@GetMapping("/estados/exibir/{id}")
