c0bda1f3ccb9620bc070bd2c6b23539af6474bad
Lucas Apoena <lucasapoena@gmail.com>
2018-04-20 16:00:41 -0300

    Manter Cidades

--- src/main/java/br/com/abutres/model/Cidade.java
+++ src/main/java/br/com/abutres/model/Cidade.java
@@ -15,9 +15,6 @@ import javax.validation.constraints.NotNull;
 @SequenceGenerator(name="seq_cidade", sequenceName="seq_cidade", initialValue=1, allocationSize=1)
 public class Cidade implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -27,7 +24,7 @@ public class Cidade implements Serializable {
 	@NotBlank
 	private String nome;
 	
-	@NotNull
+	@NotNull(message="Estado não pode ficar em branco!")
 	@ManyToOne
 	private Estado estado;
 
@@ -54,4 +51,31 @@ public class Cidade implements Serializable {
 	public void setEstado(Estado estado) {
 		this.estado = estado;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
