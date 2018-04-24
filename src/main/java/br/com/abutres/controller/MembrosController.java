1ca1d676548eb1eaa5c2bb0f057006ee1490da83
Lucas Apoena <lucasapoena@gmail.com>
2018-04-24 19:44:00 -0300

    Manter membro

--- src/main/java/br/com/abutres/controller/MembrosController.java
+++ src/main/java/br/com/abutres/controller/MembrosController.java
@@ -1,21 +1,69 @@
 package br.com.abutres.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.com.abutres.repository.MembrosRepository;
+import br.com.abutres.model.Membro;
+import br.com.abutres.service.MembrosService;
 
 @Controller
 @RequestMapping("/membros")
 public class MembrosController {
 	
 	@Autowired
-	private MembrosRepository membros;
-	
-	@GetMapping
-	public String index() {
-		return "Teste2";
+	private MembrosService membros;
+		
+	@GetMapping()
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("views/membros/listar");
+		modelAndView.addObject("membros", membros.listaTodos());
+		return modelAndView;
+	}	
+
+	@GetMapping("/adicionar")
+	public ModelAndView adicionar(Membro membro) {
+		ModelAndView modelAndView = new ModelAndView("views/membros/adicionar");
+		modelAndView.addObject(membro);
+		return modelAndView;
 	}
+	
+	@PostMapping("/adicionar")
+	public ModelAndView salvar(@Valid Membro membro, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return adicionar(membro);
+		}
+		this.membros.salvar(membro);			
+		attributes.addFlashAttribute("msg_sucess", "Membro salvo com sucesso.");
+		return new ModelAndView("redirect:/membros/");
+	}
+		
+	@GetMapping("/{id}")
+	public ModelAndView editar(@PathVariable Long id) {
+		return adicionar(membros.buscaPorId(id));
+	}	
+	
+	@DeleteMapping("/{id}")
+	public String remover(@PathVariable Long id, RedirectAttributes attributes) {
+		membros.remover(id);	
+		attributes.addFlashAttribute("msg_sucess", "Membro removido com sucesso.");
+		return "redirect:/membros";
+	}	
+	
+	@GetMapping("/exibir/{id}")
+	public ModelAndView exibir(@PathVariable("id") long id) {
+		Membro membro = membros.buscaPorId(id);
+		ModelAndView modelAndView = new ModelAndView("views/membros/exibir");
+		modelAndView.addObject("membro", membro);
+		return modelAndView;
+	}	
 }
--- src/main/java/br/com/abutres/model/Membro.java
+++ src/main/java/br/com/abutres/model/Membro.java
@@ -1,38 +1,216 @@
 package br.com.abutres.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.NumberFormat;
 
 @Entity
+@SequenceGenerator(name="seq_membro", sequenceName="seq_membro", initialValue=1, allocationSize=1)
 public class Membro implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq_membro")	
 	private Long id;
+
+	@NotBlank
+	private String nome;
+	
+	@NotBlank
 	private String apelido;
+	
+	@Size(min=10,max=10)
 	private String telefone;
+	
+	@Email
 	private String email;
-	private Patente patente;
-	private Sede sede;
+		
+	@Temporal(TemporalType.DATE)
+	@Column(name="data_nascimento")
+	private Date dataNascimento;
+	
+	@Column(name="data_admissao")
+	private Date dataAdmissao;
+	
+	@NumberFormat
+	private String cnh;
 
-	public Membro() {
+	private String veiculo;
+	
+	private String endereco;
 
+	private String motoClubeItens;
+	private String patente;	
+	private String sede;
+	private String advertencias;
+	private String punicoes;
+	private String foto;
+	private String status;
+	
+	
+	
+	
+	
+	
+	
+	
+public Date getDataAdmissao() {
+		return dataAdmissao;
 	}
 
+	public void setDataAdmissao(Date dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+
+	public String getCnh() {
+		return cnh;
+	}
+
+	public void setCnh(String cnh) {
+		this.cnh = cnh;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getMotoClubeItens() {
+		return motoClubeItens;
+	}
+
+	public void setMotoClubeItens(String motoClubeItens) {
+		this.motoClubeItens = motoClubeItens;
+	}
+
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public String getSede() {
+		return sede;
+	}
+
+	public void setSede(String sede) {
+		this.sede = sede;
+	}
+
+	public String getAdvertencias() {
+		return advertencias;
+	}
+
+	public void setAdvertencias(String advertencias) {
+		this.advertencias = advertencias;
+	}
+
+	public String getPunicoes() {
+		return punicoes;
+	}
+
+	public void setPunicoes(String punicoes) {
+		this.punicoes = punicoes;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/*	@OneToOne
+	@JoinColumn(name="patente_id")
+	private Patente patente;
+	
+	private String cnh;
+	
+	@OneToMany
+	@JoinTable(
+			name="membro_has_moto_clube_itens",
+			joinColumns= {
+					@JoinColumn(name="membro_id", referencedColumnName="id")
+			},
+			inverseJoinColumns= {
+					@JoinColumn(name="moto_clube_item_id", referencedColumnName="id")
+			}
+	)
+	private List<MotoClubeItem> motoClubeItens;
+	
+	
+	
+	@OneToMany
+	@JoinTable(
+			name="membro_has_membro_advertencias",
+			joinColumns= {
+					@JoinColumn(name="membro_id", referencedColumnName="id")
+			},
+			inverseJoinColumns= {
+					@JoinColumn(name="membro_advertencias_id", referencedColumnName="id")
+			}
+	)
+	private List<MembroAdvertencia> membroAdvertencias;
+	
+	@OneToMany(cascade= {CascadeType.REMOVE})
+	@JoinTable(
+			name="membro_has_membro_punicoes",
+			joinColumns= {
+					@JoinColumn(name="membro_id", referencedColumnName="id")
+			},
+			inverseJoinColumns= {
+					@JoinColumn(name="membro_punicoes_id", referencedColumnName="id")
+			}
+	)
+	private List<MembroPunicoes> membroPunicoes;
+	
+	private String foto;
+	
+	@Enumerated(EnumType.STRING)
+	private Status status;
+*/
 	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getApelido() {
@@ -59,20 +237,21 @@ public class Membro implements Serializable {
 		this.email = email;
 	}
 
-	public Patente getPatente() {
-		return patente;
+	public Date getDataNascimento() {
+		return dataNascimento;
 	}
 
-	public void setPatente(Patente patente) {
-		this.patente = patente;
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
 	}
 
-	public Sede getSede() {
-		return sede;
+	public String getVeiculo() {
+		return veiculo;
 	}
 
-	public void setSede(Sede sede) {
-		this.sede = sede;
+	public void setVeiculo(String veiculo) {
+		this.veiculo = veiculo;
 	}
-
+	
+	
 }
