4820823a1891a81a4924436e873102f3aac18747
Robsonprod <rbsazevedo@gmail.com>
2018-04-25 19:36:41 -0300

    model membro, create enumStatus

---
+++ src/main/java/br/com/abutres/enums/EnumStatus.java
@@ -0,0 +1,18 @@
+package br.com.abutres.enums;
+
+public enum EnumStatus {
+
+	ATIVO("Ativo"),
+	AFASTADO("Afastado"),
+	EXPULSO("Expulso");
+	
+	private String descricao;
+	
+	EnumStatus(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+}
